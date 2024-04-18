ce0e6f537f69ca068a25657d24259f4d051fe784
Mateo-Daltroy <141689517+Mateo-Daltroy@users.noreply.github.com>
2024-04-18 17:00:12 -0300

    Pre clase 1 obj

--- Taller de Programacion/Modulo Objetos/TP_ProyectoAlumnos/src/tema1/Ej01Tabla2.java
+++ Taller de Programacion/Modulo Objetos/TP_ProyectoAlumnos/src/tema1/Ej01Tabla2.java
@@ -1,7 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+1- Analice el programa Ej01Tabla2.java, que carga un vector que representa la
+tabla del 2. Luego escriba las instrucciones necesarias para:
+- generar enteros aleatorios hasta obtener el número 11. Para cada número
+muestre el resultado de multiplicarlo por 2 (accediendo al vector).
  */
 package tema1;
     import PaqueteLectura.GeneradorAleatorio;
@@ -12,13 +13,19 @@ public class Ej01Tabla2 {
      * Carga un vector que representa la tabla del 2
      */
     public static void main(String[] args) {
+        GeneradorAleatorio.iniciar();
         int DF=11;  
         int [] tabla2 = new int[DF]; // indices de 0 a 10
         int i;
+        int rand;
         for (i=0;i<DF;i++) 
             tabla2[i]=2*i;
         System.out.println("2x" + "5" + "="+ tabla2[5]);
-
+        rand = GeneradorAleatorio.generarInt(12);
+        while (rand != 11){
+            System.out.println(rand + ", " + tabla2[rand]);
+            rand = GeneradorAleatorio.generarInt(12);
+        }
+        System.out.println("Terminado.");
     }
-    
 }
---
+++ Taller de Programacion/Modulo Objetos/TP_ProyectoAlumnos/src/tema1/Ej04Oficinas.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tema1;
+
+/**
+ *
+ * @author mateo
+ */
+public class Ej04Oficinas {
+    
+    public static void main(String [] args){
+        
+    }
+    
+}
